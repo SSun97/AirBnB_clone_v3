10107ba65cfa8429b736913782f9f2eff7b3f35c
rkbrian <rkbriankong@gmail.com>
2021-06-23 16:28:34 +0000

    create flask blueprint and index file for json

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""file to create a route /status on the object app_views that returns a JSON content"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app.route('/status')
+def json_status():
+    """Return JSON file: status ok"""
+    return jsonify({"status": "OK"})
