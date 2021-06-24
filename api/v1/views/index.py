6973ae0a196b93f8851dd3c6137f626290f33a2c
ssun97 <sunmingyu.97@gmail.com>
2021-06-24 05:25:24 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,25 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def json_status():
     """Return JSON file: status ok"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Return /status route"""
+    dictcls = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    dictcls = {key: storage.count(val) for key, val in dictcls.items()}
+    return jsonify(dictcls)
