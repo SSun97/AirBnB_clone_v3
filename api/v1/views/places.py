1adf052cc235a307564b42e2370839afeed7a256
ssun97 <sunmingyu.97@gmail.com>
2021-06-25 05:28:17 +0000

    add strict_slashes=False

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -18,13 +18,15 @@ def get_all_places(city_id):
     return get_all_cls(parent_cls, city_id, "places")
 
 
-@app_views.route("/places/<place_id>", methods=["GET"])
+@app_views.route("/places/<place_id>", strict_slashes=False,
+                 methods=["GET"])
 def get_place(place_id):
     """GET /place api route"""
     return get(cls, place_id)
 
 
-@app_views.route("/places/<place_id>", methods=["DELETE"])
+@app_views.route("/places/<place_id>", strict_slashes=False,
+                 methods=["DELETE"])
 def delete_place(place_id):
     """DELETE /place api route"""
     return delete(cls, place_id)
@@ -32,13 +34,14 @@ def delete_place(place_id):
 
 @app_views.route("/cities/<city_id>/places", strict_slashes=False,
                  methods=["POST"])
-def post_place():
+def post_place(city_id):
     """POST /place api route"""
     required_data = {"user_id", "name"}
     return post(cls, parent_cls, city_id, required_data)
 
 
-@app_views.route("/places/<place_id>", methods=["PUT"])
+@app_views.route("/places/<place_id>", strict_slashes=False, 
+                 methods=["PUT"])
 def put_place(place_id):
     """PUT /place api route"""
     ignore_data = ["id", "user_id", "city_id", "created_at", "updated_at"]
